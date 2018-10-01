2eef4558464d749f6c53bb8e10889a14114daba0
valerierogalska <valerierogalska@gmail.com>
2018-10-01 17:51:25 +0200

    Created function that detects which option was chosen by the player

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let possibleChoices = [
+const possibleChoices = [
   'Rock',
   'Paper',
   'Scissors'];
 
-function computerPlay() {
+// Get randomly computer's selection
+function computerPlay(possibleChoices) {
   return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
-let playerSelection = prompt('Rock, paper or scissors?', '');
-let computerSelection = console.log(computerPlay());
+const computerSelection = computerPlay(possibleChoices);
+const playerSelection = prompt('Rock, paper or scissors?', '');
+let playerSelectionFinal;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.search(/rock/i) !== -1) {
+    return (playerSelectionFinal = possibleChoices[0]);
+  }
+  else if (playerSelection.search(/paper/i) !== -1) {
+    return (playerSelectionFinal = possibleChoices[1]);
+  }
+  else if (playerSelection.search(/scissors/i) !== -1) {
+    return (playerSelectionFinal = possibleChoices[2]);
+  }
+  else {
+    alert('You have to select either rock, paper or scissors! Try again ;)');
+    window.location.reload();
+  }
+}
+
+playRound(playerSelection, computerSelection);
+console.log(playerSelectionFinal);
